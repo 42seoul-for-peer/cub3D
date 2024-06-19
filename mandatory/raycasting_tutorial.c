8bf2d30db2ce902e7f248dc75cc6ddb59d185c15
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-06-19 18:05:21 +0900

    tmp:tmp

--- mandatory/raycasting_tutorial.c
+++ mandatory/raycasting_tutorial.c
@@ -6,12 +6,14 @@
 /*   By: hyeunkim <hyeunkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 14:56:14 by seungjun          #+#    #+#             */
-/*   Updated: 2024/06/19 17:34:38 by hyeunkim         ###   ########.fr       */
+/*   Updated: 2024/06/19 18:04:49 by hyeunkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include <stdio.h>
 
+# define MOVE_SPEED 1
 # define KEY_ESC 53
 # define KEY_W 13
 # define KEY_A 0
@@ -19,6 +21,14 @@
 # define KEY_D 2
 # define KEY_LEFT 123
 # define KEY_RIGHT 124
+
+typedef struct s_tmp
+{
+    t_info  *info;
+    t_vec   *pos;
+    t_vec   *dir;
+    t_vec   *plane;
+}   t_tmp;
 
 int dev_close(int key, void *temp) //mlx key hook (ESC)
 {
@@ -28,27 +38,41 @@ int dev_close(int key, void *temp) //mlx key hook (ESC)
 	return (0);
 }
 
-int	key_press(int key, void *data)
+int	key_press(int key, t_tmp *tmp)
 {
-    data = 0;
+    char    **scene;
+    t_vec   *pos;
+    t_vec   *dir;
+    t_vec   *plane;
+
+    scene = tmp->info->map->scene;
+    pos = tmp->pos;
+    dir = tmp->dir;
+    plane = tmp->plane;
 	if (key == KEY_ESC)
 		dev_close(53, 0);
     if (key == KEY_W)
     {
-
+		printf(">tartget: (%f, %f)\n", pos->x + dir->x * MOVE_SPEED, pos->y);
+        if(scene[(int) pos->y][(int) (pos->x + dir->x * MOVE_SPEED)] != '1')
+            pos->x += dir->x * MOVE_SPEED;
+		printf(">>target: (%f, %f)\n", pos->x, pos->y + dir->y * MOVE_SPEED);
+        if(scene[(int) (pos->y + dir->y * MOVE_SPEED)][(int) pos->x] != '1')
+            pos->y += dir->y * MOVE_SPEED;
+		printf(">>>position: (%f, %f)\n", pos->x, pos->y);
     }
-    else if (key == KEY_S)
-    {
+    // else if (key == KEY_S)
+    // {
 
-    }
-    else if (key == KEY_A)
-    {
+    // }
+    // else if (key == KEY_A)
+    // {
 
-    }
-    else if (key == KEY_D)
-    {
+    // }
+    // else if (key == KEY_D)
+    // {
         
-    }
+    // }
     return (0);
     /* 이동하기 위해 필요한 변수
     map, posX, posY, dirX, dirY, moveSpeed, rotSpeed, planeX, planeY
@@ -90,29 +114,29 @@ int	key_press(int key, void *data)
 	return (0);
 }
 
-t_vec    init_dir_vector(char flag)
+t_vec    *init_dir_vector(char flag)
 {
-    t_vec    dir;
+    t_vec    *dir;
 
     if (flag == 'N')
     {
-        dir.x = 0;
-        dir.y = -1;
+        dir->x = 0;
+        dir->y = -1;
     }
     else if (flag == 'S')
     {
-        dir.x = 0;
-        dir.y = 1;
+        dir->x = 0;
+        dir->y = 1;
     }
     else if (flag == 'W')
     {
-        dir.x = -1;
-        dir.y = 0;
+        dir->x = -1;
+        dir->y = 0;
     }
     else
     {
-        dir.x = 1;
-        dir.y = 0;
+        dir->x = 1;
+        dir->y = 0;
     }
     return (dir);
 }
@@ -121,7 +145,7 @@ t_vec    init_plane_vector(t_vec dir)
 {
     t_vec    plane;
 
-    if (dir.x == 0)
+    if (dir->x == 0)
     {
         plane.x = 1;
         plane.y = 0;
@@ -139,13 +163,11 @@ int create_color(int *colorset)
     return (colorset[0] << 16 | colorset[1] << 8 | colorset[2]);
 }
 
-#include <stdio.h>
-
 void    tutorial(t_info *info)
 {
-    t_vec    pos;        // 플레이어의 시작 위치
-    t_vec    dir;        // 초기 방향 벡터(불변)
-    t_vec    plane;      // 플레이어가 바라보는 카메라 평면(출력 화면, 불변)
+    t_vec    *pos;        // 플레이어의 시작 위치
+    t_vec    *dir;        // 초기 방향 벡터(불변)
+    t_vec    *plane;      // 플레이어가 바라보는 카메라 평면(출력 화면, 불변)
     t_vec    rayDir;     // 광선의 방향 벡터
 
     double  cameraX;        // x축 0부터 1920까지 광선을 쏠 때, 그 광선의 카메라 평면 x좌표
@@ -169,11 +191,23 @@ void    tutorial(t_info *info)
 
     bool     hit;           // 벽에 닿았는지 여부 파악
 
+    t_tmp   *tmp;
+    tmp = ft_calloc(1, sizeof(t_tmp));
+	if (!tmp)
+		print_error(sys_call, __func__, __LINE__);
+    pos = ft_calloc(1, sizeof(t_vec));
+    dir = ft_calloc(1, sizeof(t_vec));
+    plane = ft_calloc(1, sizeof(t_vec));
+    tmp->info = info;
+    tmp->pos = pos;
+    tmp->dir = dir;
+    tmp->plane = plane;
+
     screen.x = 0;
     dir = init_dir_vector(info->map->player_dir);
-    pos.x = info->map->pos.x + 0.5;
-    pos.y = info->map->pos.y + 0.5;
-	printf("position(%f, %f)\n", pos.x, pos.y);
+    pos->x = info->map->pos->x + 0.5;
+    pos->y = info->map->pos->y + 0.5;
+	printf("position(%f, %f)\n", pos->x, pos->y);
 
 	plane = init_plane_vector(dir);
     while (screen.x < 1920)
@@ -181,33 +215,33 @@ void    tutorial(t_info *info)
         hit = false;
 
         cameraX = 2 * screen.x / (double)1920 - 1; //카메라 평면에서의 x좌표
-        rayDir.x = dir.x + plane.x * cameraX;
-        rayDir.y = dir.y + plane.y * cameraX;
+        raydir->x = dir->x + plane.x * cameraX;
+        raydir->y = dir->y + plane.y * cameraX;
 
 		//플레이어가 위치한 격자의 좌표
-        map.x = pos.x;
-        map.y = pos.y;
-        delta_dist[X] = fabs(1 / rayDir.x); //x가 1 증가할 때 광선의 이동 거리
-        delta_dist[Y] = fabs(1 / rayDir.y); //y가 1 증가할 때 광선의 이동 거리
-        if (rayDir.x < 0) //왼쪽으로 이동하다 처음 만나는 x면까지의 거리
+        map.x = pos->x;
+        map.y = pos->y;
+        delta_dist[X] = fabs(1 / raydir->x); //x가 1 증가할 때 광선의 이동 거리
+        delta_dist[Y] = fabs(1 / raydir->y); //y가 1 증가할 때 광선의 이동 거리
+        if (raydir->x < 0) //왼쪽으로 이동하다 처음 만나는 x면까지의 거리
         {
             step.x = -1;
-            side_dist[X] = (pos.x - map.x) * delta_dist[X];
+            side_dist[X] = (pos->x - map.x) * delta_dist[X];
         }
-        else // side_dist[X] : raydir.x> 0인 경우 광선의 시작부터 오른쪽으로 이동하다 처음 만나는 x면까지의 거리
+        else // side_dist[X] : raydir->x> 0인 경우 광선의 시작부터 오른쪽으로 이동하다 처음 만나는 x면까지의 거리
         {
             step.x = 1;
-            side_dist[X] = (map.x + 1.0 - pos.x) * delta_dist[X];
+            side_dist[X] = (map.x + 1.0 - pos->x) * delta_dist[X];
         }
-        if (rayDir.y < 0)
+        if (raydir->y < 0)
         {
             step.y = -1;
-            side_dist[Y] = (pos.y - map.y) * delta_dist[Y];
+            side_dist[Y] = (pos->y - map.y) * delta_dist[Y];
         }
         else
         {
             step.y = 1;
-            side_dist[Y] = (map.y + 1.0 - pos.y) * delta_dist[Y];
+            side_dist[Y] = (map.y + 1.0 - pos->y) * delta_dist[Y];
         }
         while (hit == false) //DDA 알고리즘
         {
@@ -228,9 +262,9 @@ void    tutorial(t_info *info)
                 hit = true;
         }
         if (side == 0)  // y축에 평행하게 벽에 충돌
-            perpWallDist = (map.x - pos.x + (1 - step.x) / 2) / rayDir.x;
+            perpWallDist = (map.x - pos->x + (1 - step.x) / 2) / raydir->x;
         else            // x축에 평행하게 벽에 충돌
-            perpWallDist = (map.y - pos.y + (1 - step.y) / 2) / rayDir.y;
+            perpWallDist = (map.y - pos->y + (1 - step.y) / 2) / raydir->y;
         lineHeight = (int)(1080 / perpWallDist); //화면에 그려야하는 선의 길이
         drawStart = -lineHeight / 2 + 1080 / 2;
         if (drawStart < 0)
@@ -240,15 +274,15 @@ void    tutorial(t_info *info)
             drawEnd = 1080 - 1;
 
         if (side == 0)
-            wallX = pos.y + perpWallDist * rayDir.y;
+            wallX = pos->y + perpWallDist * raydir->y;
         else
-            wallX = pos.x + perpWallDist * rayDir.x;
+            wallX = pos->x + perpWallDist * raydir->x;
         wallX -= floor((wallX));
 
         tex.x = (int) (wallX * (double)(425));
-        if (side == 0 && rayDir.x > 0)
+        if (side == 0 && raydir->x > 0)
             tex.x = 425 - tex.x - 1;
-        if (side == 1 && rayDir.y < 0)
+        if (side == 1 && raydir->y < 0)
             tex.x = 425 - tex.x - 1;
 
         ratio = 1.0 * 425 / lineHeight;
@@ -268,14 +302,14 @@ void    tutorial(t_info *info)
             texPos += ratio;
             if (side == 0)
             {
-                if (rayDir.x >= 0)
+                if (raydir->x >= 0)
                     color = info->texture->west->addr[info->texture->west->height * tex.y + tex.x];
                 else
                     color = info->texture->east->addr[info->texture->east->height * tex.y + tex.x];
             }
             else
             {
-                if (rayDir.y >= 0)
+                if (raydir->y >= 0)
                     color = info->texture->north->addr[info->texture->north->height * tex.y + tex.x];
                 else
                     color = info->texture->south->addr[info->texture->south->height * tex.y + tex.x];
@@ -293,6 +327,6 @@ void    tutorial(t_info *info)
         screen.x++;
     }
     mlx_hook(info->win, 17, 0, &dev_close, 0);
-	mlx_hook(info->win, 2, 0, &key_press, 0);
+	mlx_hook(info->win, 2, 0, &key_press, tmp);
     mlx_loop(info->mlx);
 }
