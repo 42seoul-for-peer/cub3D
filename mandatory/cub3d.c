b464908f7ad8eb0d43e085bdfa5952abf27ad7d8
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-06-25 13:53:26 +0900

    feat: add leak test

--- mandatory/cub3d.c
+++ mandatory/cub3d.c
@@ -6,18 +6,24 @@
 /*   By: hyeunkim <hyeunkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 16:07:15 by hyeunkim          #+#    #+#             */
-/*   Updated: 2024/06/25 13:10:25 by hyeunkim         ###   ########.fr       */
+/*   Updated: 2024/06/25 13:53:11 by hyeunkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "key.h"
 
+void	leak_test(void)
+{
+	system("leaks cub3D");
+}
+
 int	main(int argc, char *argv[])
 {
 	int		*map_size;
 	t_info	*info;
 
+	atexit(leak_test);
 	if (argc != 2)
 		exit_with_error(argument);
 	map_size = check_format(argv[1]);
